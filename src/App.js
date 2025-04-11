8fb9e63267a44553d31a0848fc861598836c0545
igi023 <igormolnar27@gmail.com>
2025-04-11 12:31:34 +0200

    Starting to work with Axios for requests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
-const API_KEY = '6648c884';
 //http://www.omdbapi.com/t=terminator&apikey=6648c884
+console.log(process.env.REACT_APP_OMDBAPI_KEY,process.env.REACT_APP_OMDBAPI_URL);
+
+
+
 
 
 
@@ -11,22 +12,7 @@ const API_KEY = '6648c884';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <h1>fdferwfg</h1>
   );
 }
 
